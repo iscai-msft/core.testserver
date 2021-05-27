8be2919115e8d0f6972cfae39b18533a81af23b1
iscai-msft <iscai@microsoft.com>
2021-05-27 19:42:49 -0400

    remove main module

--- __main__.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-# -------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See LICENSE.txt in the project root for
-# license information.
-# -------------------------------------------------------------------------
-from core.testserver import run
-
-run()
